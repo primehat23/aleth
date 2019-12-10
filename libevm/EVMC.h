8a68aaa5511cab7fac52d64537c89e6595c236fa
Andrei Maiboroda <andrei@ethereum.org>
2019-12-10 16:33:05 +0100

    Add to EVMC class new exec method to execute without access to state
    Use it in Executive to execute precompiles.

--- libevm/EVMC.h
+++ libevm/EVMC.h
@@ -17,6 +17,10 @@ public:
     explicit EVMC(evmc_vm* _vm) noexcept;
 
     owning_bytes_ref exec(u256& io_gas, ExtVMFace& _ext, OnOpFunc const& _onOp) final;
+
+    owning_bytes_ref exec(u256& io_gas, Address const& _myAddress, Address const& _caller,
+        u256 const& _value, bytesConstRef _data, unsigned _depth, bool _isCreate,
+        bool _isStaticCall, EVMSchedule const& _schedule) final;
 };
 }  // namespace eth
 }  // namespace dev
